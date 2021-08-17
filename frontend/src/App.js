55bc3fda6f605b3fc2a123dfef358541365507dc
shivansh <shivansh.spandey@gmail.com>
2021-08-18 02:01:06 +0530

    FinalCommitMayIG

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import Form from "./components/Client/Form.js";
-import FormData from "./components/Admin/FormData.js"
+import Form from "./components/Form.js";
+import FormData from "./components/FormData.js"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://192.168.1.9:5000/api/v1/forms",
+  baseURL: "http://localhost:5000/api/v1/forms",
   headers: {
     "Content-type": "application/json"
   }
